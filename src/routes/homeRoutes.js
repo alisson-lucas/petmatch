2fab157664f1bed49423a176f33247dc4e7385dc
Alisson Lucas de Oliveira <alisson.oliveira@QSI.LOCAL>
2020-03-10 14:00:00 -0300

    inserindo icones

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Home from '../pages/Home'
 import Locais from '../pages/Locais'
@@ -11,10 +13,11 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeNavigator() {
   return (
-    <Tab.Navigator tabBarOptions >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Locais" component={Locais} options={{title: 'Locais'}} />
-      <Tab.Screen name="Perfil" component={Perfil} options={{title: 'Perfil'}} />
+    <Tab.Navigator tabBarOptions={{showIcon: true, activeTintColor: '#f05a5b'}} >
+      <Tab.Screen name="Home" component={Home} options={{tabBarIcon: () => <Ionicons name="md-home" size={32}/>}}>
+      </Tab.Screen>
+      <Tab.Screen name="Locais" component={Locais} options={{tabBarIcon: () => <Ionicons name="md-map" size={32}/>}} />
+      <Tab.Screen name="Perfil" component={Perfil} options={{tabBarIcon: () => <Ionicons name="pet" size={32}/>}} />
     </Tab.Navigator>
   );
 }
