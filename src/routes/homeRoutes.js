acd2767e1c62ca4f2e7250710b877dc660406991
Alisson Lucas de Oliveira <alisson.oliveira@qsi.local>
2020-03-11 11:52:23 -0300

    icones do tabNavigation

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import Icon from 'react-native-vector-icons/FontAwesome'
+
 
 import Home from '../pages/Home'
 import Locais from '../pages/Locais'
@@ -14,10 +14,9 @@ const Tab = createBottomTabNavigator();
 export default function HomeNavigator() {
   return (
     <Tab.Navigator tabBarOptions={{showIcon: true, activeTintColor: '#f05a5b'}} >
-      <Tab.Screen name="Home" component={Home} options={{tabBarIcon: () => <Ionicons name="md-home" size={32}/>}}>
-      </Tab.Screen>
-      <Tab.Screen name="Locais" component={Locais} options={{tabBarIcon: () => <Ionicons name="md-map" size={32}/>}} />
-      <Tab.Screen name="Perfil" component={Perfil} options={{tabBarIcon: () => <Ionicons name="pet" size={32}/>}} />
+      <Tab.Screen name="Home" component={Home} options={{tabBarIcon: () => <Ionicons name="md-home" size={32}/>, activeTintColor: '#f05a5b'}}  />
+      <Tab.Screen name="Locais" component={Locais} options={{tabBarIcon: () => <Ionicons name="md-map" size={32}/>, activeTintColor: '#f05a5b'}} />
+      <Tab.Screen name="Perfil" component={Perfil} options={{tabBarIcon: () => <Ionicons name="md-paw" size={32}/>, activeTintColor: '#f05a5b'}} />
     </Tab.Navigator>
   );
 }
